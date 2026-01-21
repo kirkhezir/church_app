7072dab46bbe0a6ec759407e132b11631faa2948
kirkhezir <kirkhezir123@gmail.com>
2026-01-21 15:04:34 +0700

    fix: suppress ban-types ESLint errors for Prisma types
    Add eslint-disable comments for legitimate use of {} in Prisma
    GetPayload generic type arguments. These are required by Prisma's
    typing system and cannot be changed to Record<string, never>.
    
    Backend lint now passes with 0 errors (180 warnings only)
    Frontend lint now passes with 0 errors (121 warnings only)

--- backend/src/types/prisma-extensions.d.ts
+++ backend/src/types/prisma-extensions.d.ts
@@ -2,13 +2,21 @@
 import { Prisma, PrismaClient } from '@prisma/client';
 
 // Type aliases for model types (used in imports like: import { Member } from '@prisma/client')
+// eslint-disable-next-line @typescript-eslint/ban-types
 export type Member = Prisma.membersGetPayload<{}>;
+// eslint-disable-next-line @typescript-eslint/ban-types
 export type Event = Prisma.eventsGetPayload<{}>;
+// eslint-disable-next-line @typescript-eslint/ban-types
 export type Announcement = Prisma.announcementsGetPayload<{}>;
+// eslint-disable-next-line @typescript-eslint/ban-types
 export type Message = Prisma.messagesGetPayload<{}>;
+// eslint-disable-next-line @typescript-eslint/ban-types
 export type EventRSVP = Prisma.event_rsvpsGetPayload<{}>;
+// eslint-disable-next-line @typescript-eslint/ban-types
 export type AuditLog = Prisma.audit_logsGetPayload<{}>;
+// eslint-disable-next-line @typescript-eslint/ban-types
 export type PushSubscription = Prisma.push_subscriptionsGetPayload<{}>;
+// eslint-disable-next-line @typescript-eslint/ban-types
 export type MemberAnnouncementView = Prisma.member_announcement_viewsGetPayload<{}>;
 
 // Extended PrismaClient type with camelCase model accessors
